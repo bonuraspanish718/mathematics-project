1528b482c62f9a675a4f28230ca012b9cd7064d3
bonuraspanish718 <bonuraspanish718@gmail.com>
2025-05-25 07:08:35 +0000

    Add 47.rs

---
+++ src/47.rs
@@ -0,0 +1,8 @@
+// Exercise 1 - Linear Equations
+fn main() {
+    let x = 3.0;
+    let y = 4.0;
+
+    println!("The solution to the equation {}*x + {} = 2*x + 5*x is {}", &x, &y, (x / (x + y)));
+}
+
